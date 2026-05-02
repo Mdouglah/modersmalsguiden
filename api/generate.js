a4b0e701819f8a61122808d1e9631d2e54d0d0e2
Mdouglah <mdouglah@hotmail.com>
2026-05-02 20:42:54 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -18,8 +18,8 @@ export default async function handler(req, res) {
       }),
     });
 
-    const text = await response.text();
-    return res.status(200).send(text);
+    const data = await response.json();
+return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
